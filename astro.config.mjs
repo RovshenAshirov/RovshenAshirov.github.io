dbc57658814b51d42b8504b4fd2838872b4f0c6e
RovshenAshirov <rovshenashirov1619@gmail.com>
2026-02-25 23:07:56 +0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rovshenashirov.github.io',
-  output: 'static',
-  build: {
-    format: 'directory'
-  },
-  vite: {
-    server: {
-      fs: {
-        strict: false
-      }
-    }
-  },
-  publicDir: './public'
+    site: 'https://rovshenashirov.github.io',
+    output: 'static',
+    build: {
+        format: 'directory'
+    },
+    vite: {
+        server: {
+            fs: {
+                strict: false
+            }
+        }
+    },
+    publicDir: './public'
 });
